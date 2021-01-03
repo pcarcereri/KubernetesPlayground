e5de5692f2090318b1d58f57de37c192006bae38
Pietro <pietro.carcereri@gmail.com>
2021-01-03 11:29:10 +0100

    Multi deplyoment

--- FileProcessor/Scripts/deploykubernetes.sh
+++ FileProcessor/Scripts/deploykubernetes.sh
@@ -1,5 +1,5 @@
 ﻿#!/bin/bash
 chmod +x ./deploykubernetes.sh
 
-echo "Deploying kubernetes-deployment.yml.."
-kubectl apply -f ./../KubernetesFiles/kubernetes-deployment.yml
+echo "Deploying kubernetes yaml files.."
+kubectl apply -f ./../KubernetesFiles/
