34f98788ffd4b540d07b715594ed0aad336b60f9
Pietro <pietro.carcereri@gmail.com>
2021-01-31 18:57:27 +0100

    Added health check to the sample Web API

---
+++ Kubernetes-MVP/App/SampleWebAPI/Startup.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace SampleWebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            // https://docs.microsoft.com/en-us/dotnet/architecture/microservices/implement-resilient-applications/monitor-app-health#implement-health-checks-in-aspnet-core-services
+            services.AddHealthChecks()
+            // Add a health check for a SQL Server database
+            .AddCheck(
+                "DB-check",
+                new SqlConnectionHealthCheck(Configuration["Database:ConnectionString"]),
+                HealthStatus.Unhealthy,
+                new string[] { "db" });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHealthChecks("/hc");
+            });
+
+            app.Run(async (context) =>
+            {
+                var message = $"Config value: {Configuration["Database:ConnectionString"]}\n" +
+                    $"Secret value: {Configuration["SomeLibrary:SomeKey"]}";
+                await context.Response.WriteAsync(message);
+            });
+
+        }
+    }
+}
